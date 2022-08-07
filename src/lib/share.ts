5c46e2cd0025e91d577a10c646028dbcb6c64795
Roger Wang <rogerwang2520@gmail.com>
2022-08-06 21:01:43 -0700

    Update shareable game link

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -8,7 +8,7 @@ const webShareApiDeviceTypes: string[] = ['mobile', 'smarttv', 'wearable']
 const parser = new UAParser()
 const browser = parser.getBrowser()
 const device = parser.getDevice()
-const gameUrl = 'rebrand.ly/verticle'
+const gameUrl = 'verticle.herokuapp.com'
 
 export const shareStatus = (
   solution: string,
