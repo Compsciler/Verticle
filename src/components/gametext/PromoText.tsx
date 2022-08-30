4637b767cff800a3006692edae7d14ca1541b628
Roger Wang <rogerwang2520@gmail.com>
2022-08-30 16:32:58 -0500

    Fix issue with promo text color on dark mode

--- src/components/gametext/PromoText.tsx
+++ src/components/gametext/PromoText.tsx
@@ -8,11 +8,10 @@ const styles = {
   fontSize: '18px',
 }
 
-const classes = '"flex justify-center mb-3 ml-2 mr-2 text-center dark:text-white"'
-const a_classes = "underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+const classes = "flex justify-center mb-3 ml-2 mr-2 text-center dark:text-white"
+// const a_classes = "underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
 
 export const PromoText = ({ text }: Props) => {
-  console.log(text)
   return (
     <div style={styles} className={classes}>
       <p>{ReactHtmlParser(text)}</p>
